4d2fe07338c9e8c225c5703b789c4c9f30e5a7ba
SSunkist <152255974+SSunkist@users.noreply.github.com>
2024-02-08 14:55:22 +0900

    수정해올것

---
+++ VoiceC.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './css/VoiceC.css';
+import homebtn from '../img/home.png';
+import AudioRecorder from './AudioRecorder';
+
+function VoiceC() {
+  const [isRecording, setIsRecording] = useState(false); // 녹화 상태를 관리하는 state
+
+  const gohome = () => {
+    window.location.href = '/';
+  };
+
+  // 녹화 상태를 변경하는 함수
+  const handleRecordingChange = (recording) => {
+    setIsRecording(recording);
+  };
+
+  return (
+    <div className={`Vbody`}>
+      <div className={`color-change-5x ${isRecording ? 'hidden' : ''}`}>
+        <div className="loadingio-spinner-ripple-y05os7hhvii">
+          <div className="ldio-fi0u2squulj">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+      <img src={homebtn} className="homebtn" onClick={gohome}></img>
+      <div className="audiobox">
+        <AudioRecorder onRecordingChange={handleRecordingChange} isRecording={isRecording} />
+      </div>
+    </div>
+  );
+}
+
+export default VoiceC;
